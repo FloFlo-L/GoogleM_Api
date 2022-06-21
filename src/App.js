8bbd2832ccf73126639a9eee8396dced4bd414da
FloFlo-L <flowfloxper@gmail.com>
2022-06-21 15:30:28 +0200

    first commit !

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+import {location} from './data/info'
+
+console.log(location)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <h1>Api google map</h1>
+      <LoadScript googleMapsApiKey='AIzaSyCOS4xy4MdXtYqxyfaL6gHivXqe8Y3f6zs'>
+        <GoogleMap
+          mapContainerStyle={{height :`700px`, width: `100%`}}
+          center={{ lat: 46.862725, lng: 2.287592 }}
+          zoom={6}
         >
-          Learn React
-        </a>
-      </header>
+          {location.map((info)=>(
+            <Marker
+              key={info.id}
+              position={{lat: info.lat, lng: info.lng}}
+              title={info.city}
+            />
+          ))}
+        </GoogleMap>
+      </LoadScript>
     </div>
+    
   );
 }
-
 export default App;
